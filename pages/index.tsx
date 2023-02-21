958b7f6af9b3e6791c6345794f1d3de8d10ac299
Taichi Iida <chiaki.621@gmail.com>
2023-02-21 11:14:04 +0900

    でもページリンク追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <div className={styles.description}>
+          <div>
+            <Link href="/demo" className={styles.card}>
+              デモページ
+            </Link>
+          </div>
+        </div>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
