14188ae1fa23ea17ad9b0a828b30c26ac92fb641
daisuke <dev_daisuke0926@yahoo.co.jp>
2023-05-29 20:33:10 +0900

    close #10 +フォルダ名誤りでtailwindが効いていなかったので修正しました。

--- component/molecules/Header.tsx
+++ component/molecules/Header.tsx
@@ -1,18 +1,15 @@
-import HeaderLink from '../atoms/HeaderLink';
 import { FC } from 'react';
 import { FaHome, FaBell, FaUser } from 'react-icons/fa';
+import HeaderLink from '../atoms/HeaderLink';
 
-//icon={<SomeIcon />}を加えればアイコンで表示できる
 const Header: FC = () => {
-    return (
-        <header className="header">
-            <div className="page-container" style={{ display: 'flex' }}>
-                <HeaderLink page="top" icon={<FaHome />} />
-                <HeaderLink page="notice" icon={<FaBell />} />
-                <HeaderLink page="account" icon={<FaUser />} />
-            </div>
-        </header>
-    );
+  return (
+    <header className="header flex space-x-4">
+      <HeaderLink page="top" icon={<FaHome />} />
+      <HeaderLink page="notice" icon={<FaBell />} />
+      <HeaderLink page="account" icon={<FaUser />} />
+    </header>
+  );
 };
 
 export default Header;
